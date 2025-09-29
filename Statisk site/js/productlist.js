8e3303be9ff9fd62e6e99965977c69a3a4080a50
Louise <lodo0001@stud.kea.dk>
2025-09-29 17:11:47 +0200

    dynamisk-forside

--- Statisk site/js/productlist.js
+++ Statisk site/js/productlist.js
@@ -1,37 +1,45 @@
-const productContainer = document.querySelector(".productContainer");
+const productlistContainer = document.querySelector(".productContainer");
 
-fetch("https://kea-alt-del.dk/t7/api/products?limit=50")
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
+const header = (document.querySelector("h1").textContent = category);
+
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=48&category=${category}`)
   .then((response) => response.json())
-  .then(showProducts);
+  .then((data) => showProducts(data));
 
 function showProducts(products) {
   products.forEach((element) => {
     productContainer.innerHTML += `
 
-      <article class="produkter ${element.soldout ? "soldout" : ""} ${
+      <article class="products ${element.soldout ? "soldout" : ""} ${
       element.discount ? "discounted" : ""
-    }">
+    }"> 
+    <a href="produkt.html?id=${element.id}" class="readmore">
         ${element.soldout ? `<div class="label">Udsolgt</div>` : ""}
+       
         <img
           src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
           alt="${element.productdisplayname}"
         />
+        <h4>${element.brandname} </h4>
         <h3>${element.productdisplayname}</h3>
-        <p class="subtle">${element.articletype} / ${element.brandname}</p>
+        <div class="discounted">
         <p class="price">DKK <span>${element.price}</span>,-</p>
         ${
           element.discount
             ? `
-          <div class="discounted">
+            /
             <div class="badge">${element.discount}%</div>
-            <p>Now DKK ${Math.round(
+            <p class="rabat">Now DKK ${Math.round(
               element.price - (element.price * element.discount) / 100
             )},-</p>
           </div>
         `
             : ""
         }
-      <a href="produkt.html?id=${element.id}" class="readmore">Read More</a>
+        <br/>
+      </a>
       </article>`;
   });
 }
